609215cd47110c0081b37033c47a03002103e343
이나경 <leena3823@gmail.com>
2024-07-22 00:37:19 +0900

    :zap: Nickname.jsx

--- TodayMom/App.js
+++ TodayMom/App.js
@@ -3,18 +3,43 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Nickname from './components/Nickname';
 import Start from './components/Start.jsx';
+import { Button } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Nickname" component={Nickname} />
-        <Stack.Screen name="Start" component={Start} />
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen
+          name="Nickname"
+          component={Nickname}
+          options={({ navigation }) => ({
+            headerTitle: '',
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image
+                  source={require('./assets/images/leftallow.png')}
+                  style={{ width: 25, height: 25 }}
+                />
+              </TouchableOpacity>
+            ),
+            headerLeftContainerStyle: {
+              paddingLeft: 20,
+            },
+            headerStyle: {
+              elevation: 0, // Android에서의 그림자 제거
+              shadowOpacity: 0, // iOS에서의 그림자 제거
+            },
+          })}
+        />
+        <Stack.Screen
+          name="Start"
+          component={Start}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- TodayMom/components/Nickname.jsx
+++ TodayMom/components/Nickname.jsx
@@ -1,65 +1,33 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
 const Nickname = () => {
-  const [selectedButton, setSelectedButton] = useState(null);
+  const [nickname, setNickname] = useState('');
 
-  const handlePress = (buttonId) => {
-    setSelectedButton(buttonId);
-  };
-
-  const getButtonStyle = (buttonId) => {
-    if (selectedButton === buttonId) {
-      return [styles.button, styles.selectedButton];
-    }
-    return styles.button;
-  };
-
-  const getTextStyle = (buttonId) => {
-    if (selectedButton === buttonId) {
-      return [styles.buttonText, styles.selectedButtonText];
-    }
-    return styles.buttonText;
-  };
-
-  const getNextButtonStyle = () => {
-    if (selectedButton) {
-      return [styles.nextButton, styles.nextButtonActive];
-    }
-    return styles.nextButton;
-  };
+  const isButtonEnabled = nickname.length > 0;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.subText}>반가워요:)</Text>
+      <Text style={styles.subText}>반가워요 :)</Text>
       <Text style={styles.subText}>어떻게 불러드릴까요?</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={getButtonStyle('mom')}
-          onPress={() => handlePress('mom')}
-        >
-          <Image
-            source={require('../assets/images/Woman.png')}
-            style={styles.image}
-            resizeMode="contain"
-          />
-          <Text style={getTextStyle('mom')}>엄마</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity
-          style={getButtonStyle('daughter')}
-          onPress={() => handlePress('daughter')}
-        >
-          <Image
-            source={require('../assets/images/Daughter.png')}
-            style={styles.image}
-            resizeMode="contain"
-          />
-          <Text style={getTextStyle('daughter')}>자녀</Text>
-        </TouchableOpacity>
-      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={setNickname}
+        value={nickname}
+        placeholder="이렇게 불러주세요!!"
+      />
 
-      <TouchableOpacity style={getNextButtonStyle()}>
+      <TouchableOpacity
+        style={[styles.nextButton, isButtonEnabled && styles.buttonActive]}
+        onPress={() => console.log('다음 단계로!')}
+      >
         <Text style={styles.nextButtonText}>다음</Text>
       </TouchableOpacity>
     </View>
@@ -74,87 +42,49 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 20,
   },
-  selectedButtonText: {
-    color: '#A30FFA',
-  },
-  nextButtonActive: {
-    backgroundColor: '#A30FFA',
-  },
-  headerText: {
-    marginTop: 88,
-    color: '#767676',
-    fontFamily: 'Pretendard',
-    fontSize: 14,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    lineHeight: 20,
-    letterSpacing: -0.35,
-    marginBottom: 4,
-  },
   subText: {
     color: '#111',
     fontFamily: 'Pretendard',
     fontSize: 24,
-    fontStyle: 'normal',
     fontWeight: '400',
     lineHeight: 34,
     letterSpacing: -0.35,
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    marginBottom: 296,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 165,
-    height: 165,
-    borderRadius: 24,
-    backgroundColor: '#F7F7FB',
-    padding: 10,
-  },
-  selectedButton: {
-    borderColor: 'rgba(163, 15, 250, 0.50)',
-    borderWidth: 1,
-    backgroundColor: 'rgba(163, 15, 250, 0.15)',
-  },
-  image: {
-    width: 56,
-    height: 56,
-    marginBottom: 12,
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#111',
+  input: {
     fontFamily: 'Pretendard',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    lineHeight: 26,
-    letterSpacing: -0.45,
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 24,
+    color: '#111',
+    borderBottomWidth: 1,
+    borderBottomColor: '#E5E5EC',
+    width: '100%',
+    marginBottom: 24,
+    paddingTop: 14,
+    marginTop: 24,
+    paddingBottom: 14,
+    marginBottom: 426,
   },
   nextButton: {
     backgroundColor: 'rgba(163, 15, 250, 0.15)',
-    paddingTop: 14,
-    paddingBottom: 14,
-    paddingLeft: 16,
-    paddingRight: 16,
+    paddingVertical: 14,
+    paddingHorizontal: 16,
     justifyContent: 'center',
     alignItems: 'center',
-    display: 'flex',
     width: '100%',
     borderRadius: 12,
   },
   nextButtonText: {
     color: '#fff',
-    textAlign: 'center',
     fontFamily: 'Pretendard',
     fontSize: 16,
-    fontStyle: 'normal',
     fontWeight: '600',
     lineHeight: 24,
+    textAlign: 'center',
     letterSpacing: -0.4,
+  },
+  buttonActive: {
+    backgroundColor: '#A30FFA',
   },
 });
 
