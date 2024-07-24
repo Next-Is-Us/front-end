636256d6cb8d6a09340c8f51b04c22c0f21563c0
Jun279 <33.beautifulboy@gmail.com>
2024-07-24 15:48:13 +0900

    Updata App.js

--- TodayMom/App.js
+++ TodayMom/App.js
@@ -1,26 +1,26 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Nickname from './screens/Nickname';
-import Start from './screens/Start.jsx';
-import { Button, StatusBar } from 'react-native';
-import { TouchableOpacity } from 'react-native';
-import { Image } from 'react-native';
-import Invite from './screens/Invite.jsx';
-import Status from './screens/Status.jsx';
-import { Text } from 'react-native';
-import { View } from 'react-native';
-import { StyleSheet } from 'react-native';
-import MomHomeScreen from './screens/MomHomeScreen.js';
-import MomRecordConditionScreen from './screens/MomRecordConditionScreen.js';
-import InfoWrite from './screens/InfoWrite.jsx';
-import NewWrite from './screens/NewWrite.js';
-import { PostProvider } from './screens/PostContext.js';
-import ViewContent from './screens/ViewContent.js';
-import MomRecordConditionAdditionScreen from './screens/MomRecordConditionAdditionScreen.js';
-import NotificationScreen from './screens/NotificationScreen.js';
-import MyPageScreen from './screens/MyPageScreen.js';
-import ChildrenHomeScreen from './screens/ChildrenHomeScreen.js';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Nickname from "./screens/Nickname";
+import Start from "./screens/Start.jsx";
+import { Button, StatusBar } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { Image } from "react-native";
+import Invite from "./screens/Invite.jsx";
+import Status from "./screens/Status.jsx";
+import { Text } from "react-native";
+import { View } from "react-native";
+import { StyleSheet } from "react-native";
+import MomHomeScreen from "./screens/MomHomeScreen.js";
+import MomRecordConditionScreen from "./screens/MomRecordConditionScreen.js";
+import InfoWrite from "./screens/InfoWrite.jsx";
+import NewWrite from "./screens/NewWrite.js";
+import { PostProvider } from "./screens/PostContext.js";
+import ViewContent from "./screens/ViewContent.js";
+import MomRecordConditionAdditionScreen from "./screens/MomRecordConditionAdditionScreen.js";
+import NotificationScreen from "./screens/NotificationScreen.js";
+import MyPageScreen from "./screens/MyPageScreen.js";
+import ChildrenHomeScreen from "./screens/ChildrenHomeScreen.js";
 
 const Stack = createStackNavigator();
 
@@ -56,7 +56,11 @@ function App() {
               component={MomHomeScreen}
               options={{ headerShown: false }}
             />
-<Stack.Screen name="ChildrenHome" component={ChildrenHomeScreen} options={{headerShown: false}} />
+            <Stack.Screen
+              name="ChildrenHome"
+              component={ChildrenHomeScreen}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen
               name="SelectCondition"
               component={MomRecordConditionScreen}
@@ -82,11 +86,11 @@ function App() {
               name="Nickname"
               component={Nickname}
               options={({ navigation }) => ({
-                headerTitle: '',
+                headerTitle: "",
                 headerLeft: () => (
                   <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Image
-                      source={require('./assets/images/leftallow.png')}
+                      source={require("./assets/images/leftallow.png")}
                       style={{ width: 25, height: 25 }}
                     />
                   </TouchableOpacity>
@@ -104,11 +108,11 @@ function App() {
               name="Invite"
               component={Invite}
               options={({ navigation }) => ({
-                headerTitle: '',
+                headerTitle: "",
                 headerLeft: () => (
                   <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Image
-                      source={require('./assets/images/leftallow.png')}
+                      source={require("./assets/images/leftallow.png")}
                       style={{ width: 25, height: 25 }}
                     />
                   </TouchableOpacity>
@@ -131,15 +135,15 @@ function App() {
 
 const styles = StyleSheet.create({
   leftHeaderContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginLeft: 20,
     paddingBottom: 14,
     paddingTop: 14,
   },
   rightHeaderContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginRight: 20,
     paddingBottom: 14,
     paddingTop: 14,
@@ -147,10 +151,10 @@ const styles = StyleSheet.create({
   icon: {},
   icon2: {
     marginLeft: 8,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   headerText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
     marginLeft: 8,
   },
