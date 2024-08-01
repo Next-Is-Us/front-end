450ac440d728b5d35d0c46bf554de8543444a4ab
이나경 <leena3823@gmail.com>
2024-08-01 21:43:58 +0900

    Edit Commuroom UI

--- TodayMom/components/CommunityPage.js
+++ TodayMom/components/CommunityPage.js
@@ -4,9 +4,34 @@ import PurpleNav from './PurpleNav';
 import Communication from '../screens/Communication';
 import InfoWrite from '../screens/InfoWrite';
 import BottomNav from './BottomNav';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const CommunityPage = () => {
   const [activeTab, setActiveTab] = useState('Communication');
+  const [roleNames, setRoleNames] = useState([]);
+
+  useEffect(() => {
+    const fetchRoleNames = async () => {
+      try {
+        const storedRoleNames = await AsyncStorage.getItem('userRoles');
+        const roles = storedRoleNames ? JSON.parse(storedRoleNames) : [];
+        setRoleNames(roles);
+
+        if (
+          !roles.includes('ROLE_MOM') &&
+          !roles.includes('ROLE_ADMIN') &&
+          !roles.includes('ROLE_DOCTOR')
+        ) {
+          setActiveTab('InfoWrite');
+        }
+      } catch (error) {
+        console.error('Failed to fetch role names from AsyncStorage', error);
+      }
+    };
+
+    fetchRoleNames();
+  }, []);
 
   return (
     <View style={styles.navsafe}>
--- TodayMom/screens/Comment.js
+++ TodayMom/screens/Comment.js
@@ -11,10 +11,108 @@ import { useState } from 'react';
 import Purplesend from '../assets/images/purplesend.svg';
 import { TouchableWithoutFeedback } from 'react-native';
 import { Keyboard } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FlatList } from 'react-native';
 
 const Comment = () => {
   const [isFocused, setIsFocused] = useState(false);
   const [text, setText] = useState('');
+  const route = useRoute();
+  const roomPostId = route.params.roomPostId;
+  const [postDetails, setPostDetails] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    // 댓글 리스트 불러옴
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    try {
+      const response = await fetch(
+        `https://15.164.134.131/api/roomComment/all/${roomPostId}?page=0`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const result = await response.json();
+      if (response.ok) {
+        setComments(result.data.data);
+        console.log('댓글:', result.data.data);
+      } else {
+        throw new Error('Failed to fetch comments');
+      }
+    } catch (error) {
+      console.error('Error fetching comments:', error);
+    }
+  };
+
+  const fetchPostDetail = async () => {
+    // 상세 게시글 불러옴
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    try {
+      const response = await fetch(
+        `https://15.164.134.131/api/roomPost/detail/${roomPostId}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const result = await response.json();
+      if (response.ok) {
+        setPostDetails(result.data);
+        console.log(result.data);
+      } else {
+        throw new Error('Failed to fetch post details');
+      }
+    } catch (error) {
+      console.error('Error fetching post details:', error);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPostDetail();
+      fetchComments();
+    }, [roomPostId])
+  );
+
+  const handleSubmitComment = async () => {
+    // 댓글 작성 api
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    const commentData = {
+      roomPostId,
+      commentContent: text,
+    };
+
+    try {
+      const response = await fetch('https://15.164.134.131/api/roomComment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify(commentData),
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        console.log('Comment submitted successfully', result);
+        setText('');
+        fetchComments(); // 댓글을 다시 불러와 갱신
+      } else {
+        throw new Error('Failed to submit comment');
+      }
+    } catch (error) {
+      console.error('Error submitting comment:', error);
+    }
+  };
 
   const handleTextChange = (newText) => {
     setText(newText);
@@ -31,59 +129,66 @@ const Comment = () => {
     Keyboard.dismiss();
   };
 
+  const renderCommentItem = ({ item }) => (
+    <View style={styles.realcomment}>
+      <Text style={styles.username}>{item.author}</Text>
+      <Text style={styles.comments}>{item.commentContent}</Text>
+    </View>
+  );
+
   return (
-    <>
-      <SafeAreaView style={styles.safeArea}>
-        <TouchableWithoutFeedback onPress={dismissKeyboard}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={{ flex: 1 }}>
-              <Header />
-              <View style={styles.content}>
-                <Text style={styles.title}>게시글 제목</Text>
-                <View style={styles.horizontalLine}></View>
-                <Text style={styles.contenttext}>게시글 내용</Text>
-                <Text style={styles.views}>조회수 00</Text>
-              </View>
-
-              <View style={styles.commentcon}>
-                <View style={styles.row}>
-                  <Message />
-                  <Text style={styles.commenttitle}>댓글</Text>
-                  <Text style={styles.number}>00</Text>
-                </View>
-
-                <View
-                  style={[
-                    styles.write,
-                    { height: getInputHeight() },
-                    isFocused ? styles.focusedBorder : null,
-                  ]}
-                >
-                  <TextInput
-                    multiline={true}
-                    style={styles.input}
-                    placeholder="댓글을 작성해주세요"
-                    placeholderTextColor="#767676"
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                    onChangeText={handleTextChange}
-                    value={text}
-                  />
-                  <TouchableOpacity>
-                    {isFocused ? <Purplesend /> : <Send />}
-                  </TouchableOpacity>
-                </View>
-
-                <View style={styles.realcomment}>
-                  <Text style={styles.username}>유저 이름123</Text>
-                  <Text style={styles.comments}>댓글 내용</Text>
-                </View>
-              </View>
+    <SafeAreaView style={styles.safeArea}>
+      <TouchableWithoutFeedback onPress={dismissKeyboard}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{ flex: 1 }}>
+            <Header />
+            <View style={styles.content}>
+              <Text style={styles.title}>{postDetails?.title}</Text>
+              <View style={styles.horizontalLine}></View>
+              <Text style={styles.contenttext}>{postDetails?.content}</Text>
+              <Text style={styles.views}>조회수 {postDetails?.viewCount}</Text>
             </View>
-          </ScrollView>
-        </TouchableWithoutFeedback>
-      </SafeAreaView>
-    </>
+
+            <View style={styles.commentcon}>
+              <View style={styles.row}>
+                <Message />
+                <Text style={styles.commenttitle}>댓글</Text>
+                <Text style={styles.number}>{comments.length}</Text>
+              </View>
+
+              <View
+                style={[
+                  styles.write,
+                  { height: getInputHeight() },
+                  isFocused ? styles.focusedBorder : null,
+                ]}
+              >
+                <TextInput
+                  multiline={true}
+                  style={styles.input}
+                  placeholder="댓글을 작성해주세요"
+                  placeholderTextColor="#767676"
+                  onFocus={() => setIsFocused(true)}
+                  onBlur={() => setIsFocused(false)}
+                  onChangeText={handleTextChange}
+                  value={text}
+                />
+                <TouchableOpacity onPress={handleSubmitComment}>
+                  {isFocused ? <Purplesend /> : <Send />}
+                </TouchableOpacity>
+              </View>
+
+              <FlatList
+                data={comments}
+                keyExtractor={(item) => item.commentId.toString()}
+                renderItem={renderCommentItem}
+                contentContainerStyle={{ paddingBottom: 50 }}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 };
 
@@ -113,7 +218,6 @@ const styles = StyleSheet.create({
   realcomment: {
     flexDirection: 'column',
     marginTop: 36,
-    marginBottom: 24,
   },
   write: {
     marginTop: 12,
