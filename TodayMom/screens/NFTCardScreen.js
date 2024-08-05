11f38c0834b4938b6040b85719d62e89f749b442
이나경 <leena3823@gmail.com>
2024-08-05 20:40:43 +0900

    :zap: 이미지 최적화 완료

--- TodayMom/screens/NFTCardScreen.js
+++ TodayMom/screens/NFTCardScreen.js
@@ -1,26 +1,46 @@
-import { ImageBackground, StyleSheet, View, Dimensions, Animated, Pressable, Text } from "react-native";
-import HeaderBack from "../components/HeaderBack";
-import NFTCard from "../components/NFTCard";
-import { useRef, useEffect, useState } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Dimensions,
+  Animated,
+  Pressable,
+  Text,
+} from 'react-native';
+import HeaderBack from '../components/HeaderBack';
+import NFTCard from '../components/NFTCard';
+import { useRef, useEffect, useState } from 'react';
+import { Image } from 'react-native';
+import { Asset } from 'expo-asset';
 
-export default function NFTCardScreen({route}) {
+export default function NFTCardScreen({ route }) {
   const info = route.params.info;
   // const deviceWidth = Dimensions.get("window").width;
   // const deviceHeight = Dimensions.get("window").height;
   const [isFlipped, setIsFlipped] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
-  const dropAnimation = useRef(new Animated.Value(-Dimensions.get("window").height)).current; // 초기 위치를 화면 위로 설정
+  const dropAnimation = useRef(
+    new Animated.Value(-Dimensions.get('window').height)
+  ).current; // 초기 위치를 화면 위로 설정
   const springAnimation = useRef(new Animated.Value(0)).current;
   const flipAnimation = useRef(new Animated.Value(0)).current;
   const shakeAnimation = useRef(new Animated.Value(0)).current;
 
+  useEffect(() => {
+    const loadAssets = async () => {
+      await Asset.loadAsync(require('../assets/images/NFTBackground.png'));
+    };
+
+    loadAssets().catch((e) => console.error('Failed to prefetch image', e));
+  }, []);
+
   const shakeInterpolate = shakeAnimation.interpolate({
     inputRange: [-1, 0, 1],
-    outputRange: ["-1deg", "0deg", "5deg"],
+    outputRange: ['-1deg', '0deg', '5deg'],
   });
 
   const shakeStyle = {
-    transform: [{rotate: shakeInterpolate}]
+    transform: [{ rotate: shakeInterpolate }],
   };
 
   const shakeCard = () => {
@@ -31,64 +51,64 @@ export default function NFTCardScreen({route}) {
       Animated.timing(shakeAnimation, {
         toValue: 0,
         duration: 200,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
       Animated.timing(shakeAnimation, {
         toValue: -1,
         duration: 200,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
       Animated.timing(shakeAnimation, {
         toValue: 1,
         duration: 500,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
       Animated.timing(shakeAnimation, {
         toValue: 0,
         duration: 500,
         delay: 100,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
     ]).start(() => {
       setIsClicked(false);
     });
-  }
+  };
 
   const frontInterpolate = flipAnimation.interpolate({
     inputRange: [0, 180],
-    outputRange: ["0deg", "180deg"],
-  })
+    outputRange: ['0deg', '180deg'],
+  });
 
   const flipToFrontStyle = {
-    transform: [{rotateY: frontInterpolate}]
+    transform: [{ rotateY: frontInterpolate }],
   };
 
   const backInterpolate = flipAnimation.interpolate({
     inputRange: [0, 180],
-    outputRange: ["180deg", "360deg"],
-  })
+    outputRange: ['180deg', '360deg'],
+  });
 
   const flipToBackStyle = {
-    transform: [{rotateY: backInterpolate}]
+    transform: [{ rotateY: backInterpolate }],
   };
 
   // card flip animation
   const flipCard = () => {
-    if(!isFlipped) {
+    if (!isFlipped) {
       Animated.spring(flipAnimation, {
         toValue: 180,
         friction: 8,
         tension: 10,
-        useNativeDriver:  true
-      }). start();
-    } 
+        useNativeDriver: true,
+      }).start();
+    }
     setIsFlipped(true);
-  }
+  };
 
   useEffect(() => {
     Animated.timing(dropAnimation, {
       toValue: 0, // 최종 위치를 현재 위치로 설정
-      duration: 1000, 
+      duration: 1000,
       useNativeDriver: true,
     }).start(() => {
       Animated.loop(
@@ -96,7 +116,7 @@ export default function NFTCardScreen({route}) {
           Animated.timing(springAnimation, {
             toValue: -30,
             useNativeDriver: true,
-            duration: 1000
+            duration: 1000,
             // speed: 5,
             // bounciness: 10,
             // friction: 5,
@@ -105,12 +125,12 @@ export default function NFTCardScreen({route}) {
           Animated.timing(springAnimation, {
             toValue: 0,
             useNativeDriver: true,
-            duration: 1000
+            duration: 1000,
             // speed: 5,
             // bounciness: 10,
             // friction: 5,
             // tension: 50
-          })
+          }),
         ])
       ).start();
     });
@@ -121,33 +141,52 @@ export default function NFTCardScreen({route}) {
   //   outputRange: [0, -30],
   // });
 
-  {/* transform 스타일을 병합해줘야 함! */}
+  {
+    /* transform 스타일을 병합해줘야 함! */
+  }
   const combinedTransformStyle = {
-    transform: [
-      ...flipToBackStyle.transform,
-      ...shakeStyle.transform,
-    ]
+    transform: [...flipToBackStyle.transform, ...shakeStyle.transform],
   };
-  
+
   return (
-    <ImageBackground source={require("../assets/images/NFTBackground.png")} style={styles.screen} deferRender={true}>
+    <ImageBackground
+      source={require('../assets/images/NFTBackground.png')}
+      style={styles.screen}
+      deferRender={true}
+    >
       <HeaderBack />
       <View style={styles.cardContainer}>
         <Pressable onPress={isFlipped ? shakeCard : flipCard}>
           {!isFlipped && (
             // <Pressable onPress={flipCard}>
-            <Animated.View style={[styles.card, flipToFrontStyle,  { transform: [{ translateY: dropAnimation }, { translateY: springAnimation }] }]}>
+            <Animated.View
+              style={[
+                styles.card,
+                flipToFrontStyle,
+                {
+                  transform: [
+                    { translateY: dropAnimation },
+                    { translateY: springAnimation },
+                  ],
+                },
+              ]}
+            >
               <NFTCard back />
             </Animated.View>
             // </Pressable>
           )}
           {isFlipped && (
             <>
-            {/* <Pressable onPress={shakeCard} style={styles.pressable}> */}
-            <Animated.View style={[styles.card, combinedTransformStyle]}> 
-              <NFTCard front info={info} isClicked={isClicked} setIsClicked={setIsClicked} />
-            </Animated.View>
-            {/* </Pressable> */}
+              {/* <Pressable onPress={shakeCard} style={styles.pressable}> */}
+              <Animated.View style={[styles.card, combinedTransformStyle]}>
+                <NFTCard
+                  front
+                  info={info}
+                  isClicked={isClicked}
+                  setIsClicked={setIsClicked}
+                />
+              </Animated.View>
+              {/* </Pressable> */}
             </>
           )}
         </Pressable>
@@ -156,16 +195,18 @@ export default function NFTCardScreen({route}) {
         )} */}
       </View>
       {isFlipped && (
-            <Text style={styles.touchText}>꽃피 NFT 카드<Text style={styles.touchText2}>를 터치해보세요</Text></Text>
-        )}
+        <Text style={styles.touchText}>
+          꽃피 NFT 카드<Text style={styles.touchText2}>를 터치해보세요</Text>
+        </Text>
+      )}
     </ImageBackground>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   cardContainer: {
     // justifyContent: "center",
@@ -173,35 +214,35 @@ const styles = StyleSheet.create({
     // position: "absolute",
     // top: deviceHeight / 2,
     // left: deviceWidth / 2
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 120,
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "center",
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'center',
     // backgroundColor: "blue",
-    height: 390
+    height: 390,
   },
   card: {
     // position: "absolute",
-    backfaceVisibility: "hidden",
+    backfaceVisibility: 'hidden',
   },
   touchText: {
     marginTop: 120,
-    color: "#FFFFFF",
+    color: '#FFFFFF',
     fontSize: 15,
-    fontWeight: "600",
+    fontWeight: '600',
     lineHeight: 22,
     letterSpacing: -0.375,
-    textAlign: "center",
+    textAlign: 'center',
     // position: "absolute",
     // alignSelf: "center",
     // bottom: -150,
   },
   touchText2: {
-    color: "#FFFFFF",
+    color: '#FFFFFF',
     fontSize: 15,
-    fontWeight: "400",
+    fontWeight: '400',
     lineHeight: 22,
     letterSpacing: -0.375,
-  }
-})
+  },
+});
